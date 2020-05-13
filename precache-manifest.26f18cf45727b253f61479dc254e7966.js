ed0476ae82694054b596f20607b8529cad4f7914
richbrill <info@brillcreative.co.uk>
2020-05-13 11:44:15 +0100

    Updates

---
+++ precache-manifest.26f18cf45727b253f61479dc254e7966.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29f10953a0226316291bc47dfd1538d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a2240853b57ecedc4ff",
+    "url": "/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "ea22c04b71b66901ebac",
+    "url": "/static/css/main.7ff93458.chunk.css"
+  },
+  {
+    "revision": "6a2240853b57ecedc4ff",
+    "url": "/static/js/2.6ed83787.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.6ed83787.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea22c04b71b66901ebac",
+    "url": "/static/js/main.2c74c403.chunk.js"
+  },
+  {
+    "revision": "a836663f7cbfb1e64305",
+    "url": "/static/js/runtime-main.7acd7713.js"
+  }
+]);
