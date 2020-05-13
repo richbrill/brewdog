89cb36f25bd1fc8b29eec41227f2a1e7d5bf2082
richbrill <info@brillcreative.co.uk>
2020-05-13 12:58:40 +0100

    Added sorting

--- src/containers/Beers/index.js
+++ src/containers/Beers/index.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 import BeerItem from './../../components/BeerItem';
 import './styles.css';
+import { orderBy } from 'lodash';
 
-export default ({ beers, onSubmit }) =>
-  <div className="beers-list">{beers.map(beer =>
-    <BeerItem
-      beer={beer}
-      key={beer.id}
-      onSubmit={onSubmit}
-    />)}
-  </div>
+export default ({ beers, onSubmit }) => {
+  const [ sort, setSort ] = useState({
+    by: 'abv',
+    direction: 'asc' 
+  });
+
+  // TODO memoize in a reselect selector
+  const sortedBeers = orderBy(beers, sort.by, sort.direction);
+
+  return (
+    <Fragment>
+      <div className="sort-btns">
+        <button onClick={() => setSort({ by: 'abv', direction: 'asc' })}>sort abv asc</button>
+        <button onClick={() => setSort({ by: 'abv', direction: 'desc' })}>sort abv desc</button>
+        <button onClick={() => setSort({ by: 'name', direction: 'asc' })}>sort name asc</button>
+        <button onClick={() => setSort({ by: 'name', direction: 'desc' })}>sort name desc</button>
+      </div>
+      <div className="beers-list">{sortedBeers.map(beer =>
+        <BeerItem
+          beer={beer}
+          key={beer.id}
+          onSubmit={onSubmit}
+        />)}
+      </div>
+    </Fragment>
+  )
+}
